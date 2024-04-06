d6855a99989e04426cf6767d07e96c2b86f7bf48
Sanjay Sahu <77436131+sanju01sahu@users.noreply.github.com>
2024-04-06 13:15:02 +0530

    first push

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -1,36 +1,8 @@
-import React from 'react'
-import icon from "../assets/icon.png"
-import company from "../assets/company.png"
-import features  from "../assets/features.png"
-import resource from "../assets/resource.png"
+
 
 const Footer = () => {
   return (
     <div>
-      {/* Footer */}
-
-   <div className="footerbox">
-    <div className="footer">
-      <div>
-        <img src={icon} alt="" />
-      </div>
-      <div>
-        <img src={company} alt="" />
-      </div>
-      <div>
-        <img src={features} alt="" />
-      </div>
-      <div id="res">
-        <img src={resource} alt="" />
-      </div>
-    </div>
-   </div>
-
-
-   <div className="lastcontainer">
-    <p>@lorem ipsum dolor sit amet consectetur adipisicing elit.
-    </p>
-   </div>
     </div>
   )
 }
--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,18 +1,14 @@
-import React from 'react'
-
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
 function Navbar() {
   return (
-    <div className='navbox'>
+    <DIV>
+      <div className='navbox'>
       <div className='navbar'>
-      <div className='logo'>
-        <a href="/"><img src="" alt="Logo" /></a>
-      </div>
+      
         <div className='homepage'>
         <a href="/">Home</a>
-        <a href="">Transactions</a>
-        <a href="/subscriptions">Subscriptions</a>
+        
         {/* <a href="">Instructions</a> */}
         <a href="">About</a>
         </div>
@@ -21,20 +17,21 @@ function Navbar() {
         </div>
       </div>
     </div>
+    </DIV>
   )
 }
 
 export default Navbar
 
 
-// const DIV= styled.div`
-//   .navbar {
-//   height: 60px;
-//   background-color: #89CFF3;
-//   color: whitesmoke;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   /* padding-left: 160px; */
-// }
-// 
+const DIV= styled.div`
+  .navbar {
+  height: 60px;
+  background-color: #89CFF3;
+  color: whitesmoke;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* padding-left: 160px; */
+}
+`
